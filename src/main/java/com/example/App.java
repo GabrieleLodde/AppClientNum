7c32b94eda4623d9849d4996605ccb36fbc3a4af
Gabriele Lodde <gabrilodde@gmail.com>
2023-10-06 23:45:46 +0200

    second commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -11,7 +11,6 @@ public class App
     {
         try{
             
-            
             //Per input tastiera
             BufferedReader inputTastiera = new BufferedReader(new InputStreamReader(System.in));
 
@@ -22,37 +21,46 @@ public class App
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             
-            boolean indovinato = false;
+            String stringaRicevuta;
+
+            //Decido il range di numeri 
+            System.out.println("Inserisci 1 per generare un numero da 1 a 100, 2 da 1 a 300, 3 da 1 a 500");
+            String rangeNumeri = inputTastiera.readLine();
+            out.writeBytes(rangeNumeri + "\n");
+
             do{
                 //Inserisco il numero
-                System.out.println("Inserisci il numero da indovinare");
-                String stringaInserita = inputTastiera.readLine();
+                System.out.println("Inserisci il numero da indovinare:");
+                String numeroInserito = inputTastiera.readLine();
 
                 //Invio risposta
-                out.writeBytes(stringaInserita + "\n");
+                out.writeBytes(numeroInserito + "\n");
 
                 //Leggo e stampo risposta
-                String stringaRicevuta = in.readLine();
+                stringaRicevuta = in.readLine();
 
                 //Controlli risposta
-                if(stringaRicevuta.equals("1")){
+                if(stringaRicevuta.equals("4")){
+                    System.out.println("Inserire un numero valido!");
+                }
+                else if(stringaRicevuta.equals("1")){
                     System.out.println("Numero inserito troppo piccolo!");
                 }
-                else if(stringaInserita.equals("2")){
+                else if(stringaRicevuta.equals("2")){
                     System.out.println("Numero inserito troppo grande!");
                 }
-                else if(stringaRicevuta.equals("3")){
-                    System.out.println("HAI INDOVINATO in " + in.readLine() + " tentativi!");
-                    indovinato = true;
-                }
-            } while(indovinato);
-            
+            } while(!stringaRicevuta.equals("3"));
+
+            System.out.println("HAI INDOVINATO in " + in.readLine() + " tentativi!");
+            System.out.println("------------");
+            System.out.println("Chiusura del client");
+
             //Chiudo la connessione
             s.close();
 
         } catch ( Exception e) {
             System.out.println(e.getMessage());
-            System.out.println("Qualcosa e' andato storto");
+            System.out.println("Qualcosa è andato storto");
             System.exit(1);
         }
     }
